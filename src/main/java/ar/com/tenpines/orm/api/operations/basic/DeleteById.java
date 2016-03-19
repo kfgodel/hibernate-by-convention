c5e3e03b67101ca8711cd7e102bc2b3c8c130518
Dario L Garcia <kfgodel@gmail.com>
2016-03-19 13:53:13 -0300

    Fix delete not deleting the entity

--- src/main/java/ar/com/tenpines/orm/api/operations/basic/DeleteById.java
+++ src/main/java/ar/com/tenpines/orm/api/operations/basic/DeleteById.java
@@ -2,8 +2,10 @@ package ar.com.tenpines.orm.api.operations.basic;
 
 import ar.com.kfgodel.nary.api.Nary;
 import ar.com.tenpines.orm.api.SessionContext;
+import ar.com.tenpines.orm.api.TransactionContext;
 import ar.com.tenpines.orm.api.entities.Persistable;
 import ar.com.tenpines.orm.api.operations.SessionOperation;
+import ar.com.tenpines.orm.api.operations.TransactionOperation;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.slf4j.Logger;
@@ -13,7 +15,7 @@ import org.slf4j.LoggerFactory;
  * This type represents the operation to delete an object in the database using its id
  * Created by kfgodel on 04/04/15.
  */
-public class DeleteById implements SessionOperation<Nary<Void>> {
+public class DeleteById implements TransactionOperation<Nary<Void>> {
     public static Logger LOG = LoggerFactory.getLogger(DeleteById.class);
 
     private Class<? extends Persistable> persistentType;
@@ -27,11 +29,11 @@ public class DeleteById implements SessionOperation<Nary<Void>> {
     }
 
     @Override
-    public Nary<Void> applyWithSessionOn(SessionContext sessionContext) {
+    public Nary<Void> applyWithTransactionOn(TransactionContext transactionContext) {
         // It seems the only efficient way to delete by id (without loading the object) is using
         // a HQL query
         String deleteHql = "DELETE FROM " + persistentType.getName() + " WHERE id = :deletedId";
-        Session session = sessionContext.getSession();
+        Session session = transactionContext.getSession();
         Query deleteQuery = session.createQuery(deleteHql);
         deleteQuery.setParameter("deletedId", deletedId);
         int affectedRows = deleteQuery.executeUpdate();
